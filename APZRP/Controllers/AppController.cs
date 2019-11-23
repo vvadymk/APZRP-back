9f1e36a1c1928bdd6e7fd8e3b7a2b9e69658164b
Vadym <kochmar.vadym@gmail.com>
2019-11-23 18:14:02 +0200

    Added user reg

---
+++ APZRP/Controllers/AppController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APZRP.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APZRP.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class AppController : ControllerBase
+    {
+        private UserManager<AppUser> _userManager;
+        private SignInManager<AppUser> _singInManager;
+
+        public AppController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _singInManager = signInManager;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<Object> PostApplicationUser(UserModel model)
+        {
+            var applicationUser = new AppUser()
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+                FullName = model.FullName
+            };
+
+            try
+            {
+                var result = await _userManager.CreateAsync(applicationUser, model.Password);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
---
+++ APZRP/Models/AppContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APZRP.Models
+{
+    public class AppContext :IdentityDbContext
+    {
+        public AppContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<AppUser> ApplicationUsers { get; set; }
+    }
+}
